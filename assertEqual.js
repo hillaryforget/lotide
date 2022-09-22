ed1b3c285fab1ce795f468427abe084547187d1d
Hillary Forget <hillaryforget@outlook.com>
2022-09-22 14:49:37 -0400

    Add template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('Assertion ✅✅✅Passed: [actual] === [expected]');
+    console.log(`Assertion ✅✅✅Passed: ${actual === expected}`);
   } else {
-    console.log('Assertion 🛑🛑🛑Failed: [actual] !== [expected]');
+    console.log(`Assertion 🛑🛑🛑Failed: ${actual !== expected}`);
   }
 };
 
