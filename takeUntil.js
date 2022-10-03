a05aa3a7117c221243dde13d369a60f5a8855e00
Hillary Forget <hillaryforget@outlook.com>
2022-10-03 14:22:48 -0400

    Complete using for of loop

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+//ASSERTARRAYSEQUAL FUNCTION
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`Assertion Passed: ${array1} === ${array2}`);
+  } else if (eqArrays(array1, array2) === false) {
+    console.log(`Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+//EQARRAYS
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//TAKEUNTIL
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let value of array) {
+    if (!callback(value)) {
+      result.push(value);
+    } else if (callback(value)) {
+      break;
+    }
+  }
+  return result;
+};
+
+//TEST
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
