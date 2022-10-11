992029c00ce639cb1e2c4a1ca77a4759532afa5b
Hillary Forget <105939848+hillaryforget@users.noreply.github.com>
2022-10-11 14:34:01 -0400

    Delete Funny.js

--- Funny.js
+++
@@ -1,9 +0,0 @@
-const myDick = ('my dick');
-const yourDick = ('your dick');
-
-if (myDick > yourDick) {
-  return;
-}
-
-console.log(myDick + ' is like super size ' +
-yourDick + ' look like two fries');
