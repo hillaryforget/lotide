6dfe602048dc3762a757e0f6053348c40f695691
Hillary Forget <hillaryforget@outlook.com>
2022-10-12 14:52:09 -0400

    Refractor with mocha

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,21 @@
-const eqArrays = require('../eqArrays');
+//const eqArrays = require('../eqArrays');
+
+const assert = require('chai').assert;
+const eqArrays   = require('../eqArrays');
+
+describe("#eqArrays", () => {
+  it("returns 'true' for ['1', '2', '3'], ['1', '2', '3']", () => {
+    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+  });
+});
+
+it("returns 'false' for ['1', '2', '3'], ['3', '2', '1']", () => {
+  assert.strictEqual(eqArrays(['1', '2', '3'], ['3', '2', '1']), false);
+});
 
 // EQARRAYSTEST
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
